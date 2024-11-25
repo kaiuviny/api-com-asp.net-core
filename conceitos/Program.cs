5c364942dd12ea79f33d1e0697d3f8f3b0489636
Caio Nolasco <kaiuviny@gmail.com>
2024-11-24 22:01:46 -0300

    commit initial

---
+++ conceitos/Program.cs
@@ -0,0 +1,47 @@
+using conceitos;
+using conceitos.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new []
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+    //new Summary { Title = "",}
+};
+
+app.MapGet("/weatherforecast", () => 
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecastController
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
